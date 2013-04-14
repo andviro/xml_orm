803019e3e4e10399b9b1ba415e3db84b83674a1a
Andrew Rodionoff <andviro@gmail.com>
2013-04-14 19:17:32 +0400

    перевел тесты на фреймворк `nose`

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 from xml_orm import core
+from nose.tools import raises
 
 
 class Document(core.Schema):
-    uid = core.SimpleField(u'ИД')
-    name = core.SimpleField(u'ИмяФайла')
+    uid = core.IntegerField(u'ИД')
+    abzats = core.SimpleField(minOccurs=0)
+    name = core.CharField(u'ИмяФайла', max_length=255)
 
     class Meta:
         root = u'Документ'
@@ -15,13 +17,15 @@ class Document(core.Schema):
 class Author(core.Schema):
     name = core.SimpleField(u'Имя')
     surname = core.SimpleField(u'Фамилия')
+    is_poet = core.BooleanField(u'Поэт')
 
     class Meta:
         root = u'Автор'
 
 
 class Signature(core.Schema):
-    uid = core.SimpleField(u'@ИД', qualify=True)
+    uid = core.CharField(u'@ИД', max_length=32, qualify=True)
+    probability = core.FloatField(u'Вероятность')
     surname = core.SimpleField()
 
     class Meta:
@@ -30,12 +34,12 @@ class Signature(core.Schema):
 
 
 class Book(core.Schema):
-    uid = core.SimpleField(u'@ИД')
+    uid = core.CharField(u'@ИД', max_length=32)
     auth = core.ComplexField(Author)
-    docs = core.ComplexField(Document, minOccurs=0, maxOccurs='unbounded',
-                             use_schema_ns=False)
-    signer = core.ComplexField(Signature, minOccurs=0, use_schema_ns=False)
-    abzats = core.SimpleField(u'Абзац')
+    doc = core.ComplexField(Document, minOccurs=0, maxOccurs='unbounded',
+                            use_schema_ns=False)
+    signer = core.ComplexField(Signature, minOccurs=0, use_schema_ns=True)
+    price = core.DecimalField(u'Цена', decimal_places=2, max_digits=10)
 
     class Meta:
         root = u'Книга'
@@ -51,19 +55,41 @@ class Article(Book):
     class Meta:
         root = u'Статья'
         namespace = u'http://www.example.com/ns1'
-        encoding = u'cp1251'
-        pretty_print = True
+        encoding = u'utf-8'
+        pretty_print = False
 
-    def f(self):
-        return str(self)
 
-a = Article(uid=1, auth=u'Иван')
-a.abzats = u'Абзац'
-a.izdat = u'Мурзилка'
-a.docs.append(Document(uid=1, name='xxx'))
-a.docs.append(Document(uid=2, name='yyy'))
-a.signer = Signature(surname=u'Большой начальник', uid=100)
-xml_string = str(a)
-print unicode(a)
-b = Article.load(xml_string)
-assert unicode(a) == unicode(b)
+def test_all_fields():
+    a = Article(uid=1, auth=u'Иван')
+    a.izdat = u'Мурзилка'
+    a.price = 1.3
+    a.doc.append(a.Doc(uid=1, name='xxx', abzats=u'абзац'))
+    a.doc.append(a.Doc(uid=2, name='yyy'))
+    a.signer = a.Signer(surname=u'Большой начальник', uid=100, probability=0.4)
+    b = Article.load(str(a))
+    assert unicode(a) == unicode(b)
+
+
+@raises(AssertionError)
+def test_max_length():
+    d = Document(uid=1, abzats='text', name='a' * 256)
+    str(d)
+
+
+@raises(ValueError)
+def test_float():
+    d = Signature(surname=u'Большой начальник', uid=100, probability="asldkasjd")
+    str(d)
+
+
+@raises(ValueError)
+def test_int():
+    d = Document(uid="not int", abzats='text', name='a' * 255)
+    str(d)
+
+
+def test_bool():
+    a = Author(name='monty',
+               surname='python',
+               is_poet=False)
+    assert u'false' in unicode(a)
--- testfns.py
+++ testfns.py
@@ -3,45 +3,67 @@
 
 from uuid import uuid4
 from xml_orm.schemas.fns import ContainerFNS
+import os
 
-# Создание контейнера "с нуля"
-# в параметрах конструктору можно передавать начальные значения полей
-# для непереданных полей присваиваются значения по умолчанию
-# неприсвоенные поля без умолчаний бросят исключение при попытке вызвать
-# .save() или преобразовать контейнер в XML.
-ti = ContainerFNS(uid=uuid4().hex)
-ti.sender = ContainerFNS.Sender(uid=uuid4().hex)
-ti.receiver = ti.Receiver(uid=uuid4().hex)
-ti.sos = ti.Sos(uid=u'2AE')
-for n in range(3):
-    uid = uuid4().hex
-    doc = ti.Doc(
-        uid=uid,
-        orig_filename=uid + '.xml',
-        content=ti.Doc.Content(filename=(uid + '.bin')))
-    print unicode(doc)
-    # Добавление дескриптора документа к дескриптору контейнера
-    ti.doc.append(doc)
-    # Добавление собственно файла к содержимому контейнера
-    ti.add_file(doc.content.filename, 'test document content')
-    for k in range(2):
-        sig = doc.Signature(filename=(uuid4().hex + '.bin'))
-        # Добавление дескриптора подписи к дескриптору документа
-        doc.signature.append(sig)
-        # Добавление файла подписи к содержимому контейнера
-        ti.add_file(sig.filename, 'test signature content')
+ti = None
+extra_package = ''
 
-# сохранение сработает, только если контейнер сформирован корректно
-ti.save()
-print ti.package
-# загрузка контейнера из файла производится по имени, либо из байтовой
-# строки содержимого, либо из открытого файлового объекта
-with ContainerFNS.load(ti.package) as newti:
-    # добавления и изменения структуры отразятся на сохраненном
-    # содержимом архива
-    newti.add_file('lalalala', 'lalalala')
-    # если не изменить поле 'package', файл архива перезапишет исходный
-    # загруженный файл. Для вновь созданных контейнеров это поле
-    # пустое, и имя пакета формируется автоматически.
-    newti.package = 'some_other.zip'
-    # выход из контекста сохраняет архив
+
+def setup_module():
+    global ti
+    # Создание контейнера "с нуля"
+    # в параметрах конструктору можно передавать начальные значения полей
+    # для непереданных полей присваиваются значения по умолчанию
+    # неприсвоенные поля без умолчаний бросят исключение при попытке вызвать
+    # .save() или преобразовать контейнер в XML.
+    ti = ContainerFNS(uid=uuid4().hex)
+    ti.sender = ContainerFNS.Sender(uid=uuid4().hex)
+    ti.receiver = ti.Receiver(uid=uuid4().hex)
+    ti.sos = ti.Sos(uid=u'2AE')
+    for n in range(3):
+        uid = uuid4().hex
+        doc = ti.Doc(
+            uid=uid,
+            orig_filename=uid + '.xml',
+            content=ti.Doc.Content(filename=(uid + '.bin')))
+        print unicode(doc)
+        # Добавление дескриптора документа к дескриптору контейнера
+        ti.doc.append(doc)
+        # Добавление собственно файла к содержимому контейнера
+        ti.add_file(doc.content.filename, 'test document content')
+        for k in range(2):
+            sig = doc.Signature(filename=(uuid4().hex + '.bin'))
+            # Добавление дескриптора подписи к дескриптору документа
+            doc.signature.append(sig)
+            # Добавление файла подписи к содержимому контейнера
+            ti.add_file(sig.filename, 'test signature content')
+
+
+def teardown_module():
+    global ti, extra_package
+    os.unlink(ti.package)
+    if os.path.isfile(extra_package):
+        os.unlink(extra_package)
+
+
+def test_save():
+    global ti
+    # сохранение сработает, только если контейнер сформирован корректно
+    ti.save()
+    assert hasattr(ti, 'package')
+
+
+def test_load():
+    global ti, extra_package
+    # загрузка контейнера из файла производится по имени, либо из байтовой
+    # строки содержимого, либо из открытого файлового объекта
+    with ContainerFNS.load(ti.package) as newti:
+        # добавления и изменения структуры отразятся на сохраненном
+        # содержимом архива
+        newti.add_file('lalalala', 'lalalala')
+        # если не изменить поле 'package', файл архива перезапишет исходный
+        # загруженный файл. Для вновь созданных контейнеров это поле
+        # пустое, и имя пакета формируется автоматически.
+        newti.package = extra_package = '{0}.zip'.format(uuid4().hex)
+        # выход из контекста сохраняет архив
+    assert os.path.isfile(extra_package)
