e6c84f373ee971af5e9293411f394ae19c902964
Andrew Rodionoff <andviro@gmail.com>
2013-04-14 20:17:55 +0400

    покрыл тестами контейнер РосСтат и автозагрузку

--- testfns.py
+++ testfns.py
@@ -3,6 +3,7 @@
 
 from uuid import uuid4
 from xml_orm.schemas.fns import ContainerFNS
+from xml_orm.schemas.auto import autoload
 import os
 
 ti = None
@@ -16,27 +17,28 @@ def setup_module():
     # для непереданных полей присваиваются значения по умолчанию
     # неприсвоенные поля без умолчаний бросят исключение при попытке вызвать
     # .save() или преобразовать контейнер в XML.
-    ti = ContainerFNS(uid=uuid4().hex)
-    ti.sender = ContainerFNS.Sender(uid=uuid4().hex)
-    ti.receiver = ti.Receiver(uid=uuid4().hex)
-    ti.sos = ti.Sos(uid=u'2AE')
-    for n in range(3):
-        uid = uuid4().hex
-        doc = ti.Doc(
-            uid=uid,
-            orig_filename=uid + '.xml',
-            content=ti.Doc.Content(filename=(uid + '.bin')))
-        print unicode(doc)
-        # Добавление дескриптора документа к дескриптору контейнера
-        ti.doc.append(doc)
-        # Добавление собственно файла к содержимому контейнера
-        ti.add_file(doc.content.filename, 'test document content')
-        for k in range(2):
-            sig = doc.Signature(filename=(uuid4().hex + '.bin'))
-            # Добавление дескриптора подписи к дескриптору документа
-            doc.signature.append(sig)
-            # Добавление файла подписи к содержимому контейнера
-            ti.add_file(sig.filename, 'test signature content')
+    with ContainerFNS(uid=uuid4().hex) as ti:
+        # сохранение сработает, только если контейнер сформирован корректно
+        ti.sender = ContainerFNS.Sender(uid=uuid4().hex)
+        ti.receiver = ti.Receiver(uid=uuid4().hex)
+        ti.sos = ti.Sos(uid=u'2AE')
+        for n in range(3):
+            uid = uuid4().hex
+            doc = ti.Doc(
+                uid=uid,
+                orig_filename=uid + '.xml',
+                content=ti.Doc.Content(filename=(uid + '.bin')))
+            print unicode(doc)
+            # Добавление дескриптора документа к дескриптору контейнера
+            ti.doc.append(doc)
+            # Добавление собственно файла к содержимому контейнера
+            ti.add_file(doc.content.filename, 'test document content')
+            for k in range(2):
+                sig = doc.Signature(filename=(uuid4().hex + '.bin'))
+                # Добавление дескриптора подписи к дескриптору документа
+                doc.signature.append(sig)
+                # Добавление файла подписи к содержимому контейнера
+                ti.add_file(sig.filename, 'test signature content')
 
 
 def teardown_module():
@@ -46,11 +48,10 @@ def teardown_module():
         os.unlink(extra_package)
 
 
-def test_save():
+def test_autoload():
     global ti
-    # сохранение сработает, только если контейнер сформирован корректно
-    ti.save()
-    assert hasattr(ti, 'package')
+    newti = autoload(ti.package)
+    assert isinstance(newti, ContainerFNS)
 
 
 def test_load():
--- teststat.py
+++ teststat.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from xml_orm.schemas.pfr import ContainerPFR
+from xml_orm.schemas.stat import ContainerStat
 from xml_orm.schemas.auto import autoload
 from uuid import uuid4
 import os
 
-pfr = None
+stat = None
 
 
 def setup_module():
-    global pfr
+    global stat
     # Создание контейнера "с нуля"
     # в параметрах конструкторы можно передавать начальные значения полей
     # для непереданных полей присваиваются значения по умолчанию
     # неприсвоенные поля без умолчаний бросят исключение при попытке вызвать
     # .save() или преобразовать контейнер в XML.
-    pfr = ContainerPFR(uid=uuid4().hex)
-    pfr.sender = pfr.Sender(uid='123-456-789012')
-    pfr.skzi = pfr.Skzi(uid=uuid4().hex)
-    pfr.receiver = pfr.Receiver(uid='123-534')
-    pfr.sos = pfr.Sos(uid=u'2AE')
+    stat = ContainerStat(uid=uuid4().hex)
+    stat.doc_type = u'отчетСтат'
+    stat.transaction = u'протоколИзвещение'
+    stat.sender = stat.Sender(uid=uuid4().hex, type=u'респондент', name=u'рога и копыта')
+    stat.receiver = stat.Receiver(uid=uuid4().hex, type=u'органФСГС')
+    stat.sos = stat.Sos(uid=u'2AE', type=u'оператор')
     for n in range(3):
-        doc = pfr.Doc()
+        doc = stat.Doc()
         doc.uid = uuid4().hex
         doc.orig_filename = doc.uid + '.xml'
         doc.content = doc.Content(filename=(doc.uid + '.bin'))
+        doc.compressed = doc.encrypted = True
+        doc.content_type = u'xml'
+        doc.type = u'отчет'
         # Добавление дескриптора документа к дескриптору контейнера
-        pfr.doc.append(doc)
+        stat.doc.append(doc)
         # Добавление собственно файла к содержимому контейнера
-        pfr.add_file(doc.content.filename, 'test document content')
+        stat.add_file(doc.content.filename, 'test document content')
         for k in range(2):
             sig = doc.Signature(filename=(uuid4().hex + '.bin'))
             # Добавление дескриптора подписи к дескриптору документа
             doc.signature.append(sig)
             # Добавление файла подписи к содержимому контейнера
-            pfr.add_file(sig.filename, 'test signature content')
+            stat.add_file(sig.filename, 'test signature content')
 
 
 def teardown_module():
-    os.unlink(pfr.package)
+    os.unlink(stat.package)
 
 
 def test_save_load():
-    global pfr
-    pfr.save()
-    newpfr = autoload(pfr.package)
-    assert isinstance(newpfr, ContainerPFR)
+    global stat
+    stat.save()
+    newstat = autoload(stat.package)
+    assert isinstance(newstat, ContainerStat)
