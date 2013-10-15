06b5666d9a15300b0fb51848e450bc3928005ae6
Andrew Rodionoff <andviro@gmail.com>
2013-10-15 13:04:25 +0400

    переделал инсталлятор

--- inspector.py
+++ inspector.py
@@ -14,21 +14,21 @@ else:
     bytes = str
 
 
-def main(argv):
-    if len(argv) < 3:
+def main():
+    if len(sys.argv) < 3:
         print('''Usage: inspect.py schema1.xsd [schema2.xsd ...] result.py
 or: inspect.py schema1.xsd [schema2.xsd ...] - ''')
         sys.exit(1)
-    resfile = argv[-1]
+    resfile = sys.argv[-1]
     if resfile != '-' and not resfile.endswith('.py'):
         print('''Target file extension must be .py''')
         sys.exit(2)
     result = codecs.open(resfile, 'wb', encoding='utf-8') if resfile != '-' else sys.stdout
     result.write('# coding: utf-8\n')
-    for xsd in argv[1:-1]:
+    for xsd in sys.argv[1:-1]:
         for res in inspect_xsd(unicode(xsd)):
             s = res.reverse()
             result.write(s)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
